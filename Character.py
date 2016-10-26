4f760c1a43dd46c8f89e224ef64ea5ee277b95b9
Phil <phil.sparkes95@gmail.com>
2016-10-26 15:27:07 +0100

    Added shooting function to the character and changed the main a little bit, also added comments

--- Character.py
+++ Character.py
@@ -1,14 +1,47 @@
-import pygame
+import pygame, math
 
-pygame.transform.rotate(Character, 90)
-pygame.display.flip()
+(MouseX, MouseY) = pygame.mouse.get_pos()     # Gets the position of the mouse
+MouseX2 = MouseX / 7.11111111111111111111111111111111     #Used for rotation tracking mouse
 
-(MouseX, MouseY) = pygame.mouse.get_pos()
-MouseX = MouseX / 7.11111111111111111111111111111111
+if MouseY < 360:              #makes it rotate all the way around
+    MouseX2 = 360 - MouseX2
 
-if MouseY < 360:
-    MouseX = 360 - MouseX
+Character2 = pygame.transform.rotate(Character, MouseX2 + 90)     # The code that does the rotation
 
-Character2 = pygame.transform.rotate(Character, MouseX + 90)
+MousePress,lele,lele = pygame.mouse.get_pressed()       #checks if mouse button 1 is pressed, ignore the "lele"
 
+if MousePress ==1:           #If Mouse button 1 is pressed
+    BallSpawn = True
+    BallPosX = Width/2       #Gets the initial position of the ball
+    BallPosY = Height / 2
+    BallDestX = (MouseX - Width/2)           #Gets the Destination of the ball
+    BallDestY = (MouseY - Height/2)
+    BallXSpeed = 0             #sets the speed of the ball
+    BallYSpeed = 0
+
+    if math.sqrt(BallDestX**2) > math.sqrt(BallDestY**2):       #Checks which value is bigger, the math is there just to convert them to a positive integers
+        BallXSpeed = 20            #Sets ball speed
+        BallYSpeed = int(math.sqrt(BallDestY**2)/20)
+    else:
+        BallYSpeed = 20            #sets ball speed
+        BallXSpeed =  int(math.sqrt(BallDestX**2) / 20)
+
+if BallSpawn is True:
+
+    if -BallXSpeed/2 <= BallDestX  <= BallXSpeed/2 or -BallYSpeed/2 <= BallDestY  <=  BallYSpeed/2:  #Checks if ball has reaches destination
+        pass
+    else:                 #this is the main update making the ball move
+        if BallDestX > 0:
+            BallPosX = BallPosX + BallXSpeed
+            BallDestX = BallDestX - BallXSpeed
+        else:
+            BallPosX = BallPosX - BallXSpeed
+            BallDestX = BallDestX + BallXSpeed
+
+        if BallDestY > 0:
+            BallPosY = BallPosY + BallYSpeed
+            BallDestY = BallDestY - BallYSpeed
+        else:
+            BallPosY = BallPosY - BallYSpeed
+            BallDestY = BallDestY + BallYSpeed
 
--- Main.py
+++ Main.py
@@ -1,20 +1,28 @@
 import pygame,sys
 from pygame.locals import *
-Running = True
-Width = 1280
-Height = 720
-Y = 0
-X = 0
-XDirection = 0
-YDirection = 0
-White = (255,255,255)
-Background = pygame.image.load("maptest.png")
-Character = pygame.image.load("character.png")
-screen = pygame.display.set_mode((1280, 720))
 
+Running = True   #Needed for the main loop
+
+Width = 1280     #Screen size
+Height = 720
+
+Y = 0   #For the character movement and map tracking
+X = 0
+XDirection = 0   # 3 states, 0 = not moving, 1 = moving one direction, 2 = moving opposite direction
+YDirection = 0   # 3 states, 0 = not moving, 1 = moving one direction, 2 = moving opposite direction
+
+White = (255,255,255)      # Defines the colour white
+Red = (255,100,100)     #Defines the colour Red for later use
+Background = pygame.image.load("maptest.png")      #Map Image, Placeholder
+Character = pygame.image.load("character.png")     #Character image, Currently just a placeholder
+screen = pygame.display.set_mode((Width, Height))      #Draws the screen
+Clock = pygame.time.Clock()   #Adds the clock
+
+BallSpawn = False  #For the paintball spawn
 
 #MainLoop
 while Running:
+    Clock.tick(60)     #60 fps
     for event in pygame.event.get():
             if event.type == QUIT:
                 Running = False
@@ -29,7 +37,7 @@ while Running:
             if event.type == KEYDOWN and event.key == K_a: #On keypress w it will move the screen down
                 XDirection = 2
 
-    if YDirection is 1:
+    if YDirection is 1:       #Changes the direction depending on key pressed
         Y = Y - 1
     elif YDirection is 2:
         Y = Y + 1
@@ -37,12 +45,16 @@ while Running:
         X = X - 1
     elif XDirection is 2:
         X = X + 1
-    execfile("Character.py")
-    screen.fill(White)
-    screen.blit(Background, (X, Y))
-    screen.blit(Character2, ((Width / 2 - 32), (Height / 2 - 32)))
 
-    pygame.display.flip()
+    execfile("Character.py")  #Executes the Character python file
+    screen.fill(White)   #Fills Screen white
+    screen.blit(Background, (X, Y))     #Draws the background in relation to the player
+    screen.blit(Character2, (Width/2 - 32, Height/2 - 32))    #Draws the character in the center of the screen
+
+    if BallSpawn is True:     #Draws a paintball if conditions are met
+        pygame.draw.circle(screen, Red, (BallPosX, BallPosY), 5)
+
+    pygame.display.flip()       #Updates the display
 
 pygame.QUIT()      #When Running is not true it will quit
 sys.exit()
